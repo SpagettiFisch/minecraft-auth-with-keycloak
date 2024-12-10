499ac9cce0ffb3ef0c592ba6fc2abc9a9188df9b
SpagettiFisch <63868515+SpagettiFisch@users.noreply.github.com>
2024-12-10 15:56:44 +0100

    implemented import of index.html (also added some example index.html)

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
+import os
 from flask import Flask
 
+from modules import init
+
+conf = init.config('web')
+
+redirect_url = conf.get_redirect_url()
+
+# app = Flask(__name__) # to make the app run without any
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Index Page"
+    #TODO there should be someting like emplates in flask, so you just return the function with the html file without opening it manually first :D
+    with open("Include/index.html", 'r') as p:
+        page = p.read()
+    return page
 
-@app.route("/auth/oidc/callback")
+@app.route(f"/{redirect_url}")
 def callback():
     return "not yet implemented :("
